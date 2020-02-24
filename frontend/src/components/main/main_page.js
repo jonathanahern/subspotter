306096fa3aade66318d9cc9a59b27a2d0cb098ec
dohmain <jdoh29@gmail.com>
2020-02-24 15:01:36 -0500

    implement frontend auth

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>SubSpotter</h1>
+        <footer>
+          Copyright &copy; 2020 TripleJProductions
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
