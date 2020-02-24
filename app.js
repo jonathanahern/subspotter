aa93de8d58103143ce5e2a3b4fa5b4373c4d3eb9
jwang9392 <jwang9392@gmail.com>
2020-02-24 10:04:10 -0500

    Create mongoDB connection

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Second Hello World"));
 
