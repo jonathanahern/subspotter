adad2f21c7f03231b30a4361e0cea9471f09dcad
dohmain <jdoh29@gmail.com>
2020-02-25 10:42:06 -0500

    bring store to window

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import Root from './components/root';
-
 import configureStore from './store/store';
-
 import jwt_decode from 'jwt-decode';
-
 import { setAuthToken } from './util/session_api_util';
-
 import { logout } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
-
   if (localStorage.jwtToken) {
-
     setAuthToken(localStorage.jwtToken);
 
     const decodedUser = jwt_decode(localStorage.jwtToken);
-
     const preloadedState = { session: { isAuthenticated: true, user: decodedUser } };
 
     store = configureStore(preloadedState);
@@ -33,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     store = configureStore({});
   }
+  window.store = store;
   const root = document.getElementById('root');
-
   ReactDOM.render(<Root store={store} />, root);
 });
